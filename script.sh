e1b52fd4ef9345e4b125d44488c5d9c973de6cd5
Leonardo Kaplan <contato.leonardokaplan@gmail.com>
2015-06-14 17:52:03 -0300

    testes ok

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#! /bin/bash
 
 make
 
@@ -8,14 +8,18 @@ make
 #         varia pagina(8,16,32)
 #                pega write e fault                    
 # 2*3*3*8*4 = 576 data points
-modes=('LRU' 'NRU' 'SEG')
-memories=(128 256 512 1024 2048 4096 8192 16384)
-pages=(8 16 32)
-for i in logs/*.log do
-    for mode in modes do
-        for memory in memories do
-            for page in pages do
-                sim-virtual $mode $i $page $memory
+declare -a modes=('LRU' 'NRU' 'SEG')
+declare -a memories=( 128 256 512 1024 2048 4096 8192 16384 )
+declare -a pages=( 8 16 32 )
+for i in logs/*.log 
+do
+    for mode in "${modes[@]}" 
+    do
+        for memory in "${memories[@]}" 
+        do
+            for page in "${pages[@]}" 
+            do
+                ./sim-virtual $mode $i $page $memory
             done
         done
     done
